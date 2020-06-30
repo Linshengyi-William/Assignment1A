4177c7a4fcdaee7f1afad1f6e9fa0e383084b5c4
Linshengyi-William <67583611+Linshengyi-William@users.noreply.github.com>
2020-06-30 12:00:11 -0700

    Minor update of Q3.c
    Fixed some minor problems. The first version's printing problem is still unfixed.

--- Assignment_1A_files/Q3.c
+++ Assignment_1A_files/Q3.c
@@ -70,15 +70,26 @@ int is_all_symbol(char* word)
 		}
     ++i;
 	}
-  printf("Symbol_num: %d \n", symbol_num);
-  printf("String length: %d \n", length);
-
-  if(symbol_num == length) // No "+1" since '\n' is non-letter.
+  
+  if(symbol_num == length) // No "+1" since '\0' is non-letter.
     return 0;
   else
     return 1;
-  
+}
 
+int is_symbol_in_between(char* word)
+{
+  int length = stringLength(word);
+  int true_or_false = 0;
+  for(int i = 1; i < length - 2; i++)
+  {
+    if(!(word[i] >= 65 && word[i] <= 90) && !(word[i] >= 97 && word[i] <= 122))
+    {
+      true_or_false = 1;
+      break;
+    }
+  }
+  return true_or_false;
 }
 void convert_to_underscore_and_lowercase(char* word) 
 {
@@ -101,8 +112,8 @@ void convert_to_underscore_and_lowercase(char* word)
 void convert_to_letter_group_underscore(char* word)
 {
   int length = stringLength(word);
-  int new_word_length = length;
-  char new_word[1000];
+  //int new_word_length = length;
+  char new_word[100];
   int i = 0;
   int new_i = 0;
   while(i != length)
@@ -120,15 +131,18 @@ void convert_to_letter_group_underscore(char* word)
     }
     ++i;
   }
+  //int new_word_length = stringLength(new_word);
+  //new_word[new_word_length-1] = '\0';
   /*
   if(new_word[new_i] == '_' &&new_word[new_i+1] == '\0')
   {
     new_word[new_i] = '\0';
   }
-  */
+  
   //printf("New word length: %d \n", new_word_length);
-
+  */
   printf("After first converting: %s \n", new_word);
+  
 
 
 }
@@ -137,40 +151,43 @@ void camelCase(char* word)
 	// Convert to camelCase
 	
 	//Check if the string is valid
-	if(is_all_letter(word) == 0 || is_all_symbol(word) == 0){
+	if(is_all_letter(word) == 0 || is_all_symbol(word) == 0 || is_symbol_in_between(word) == 0)
+  {
 		printf("Invalid string. \n");
-		return;
 	}
-
+  else
 	/*Convert all non letter character to underscores;
     convert all letter character to lowercase. */
-	convert_to_underscore_and_lowercase(word);
-
-  //convert the string to the format of lettergroup + underscore + lettergroup
-  convert_to_letter_group_underscore(word);
-
-	//Start to convert it to the final version
-	int t, length = stringLength(word);
-  int i = 0;
-	for(int i = t = 0; i<length;++i)
 	{
-    if(word[i] == '_')
-		{
-      word[i+1] = toUpperCase(word[i+1]);
-			word[i] = '\0';
-		}
-    else
-		{
-			word[t++] = word[i];
-		}
+    convert_to_underscore_and_lowercase(word);
+
+    //convert the string to the format of lettergroup + underscore + lettergroup
+    convert_to_letter_group_underscore(word);
+
+	  //Start to convert it to the final version
+	  int t, length = stringLength(word);
+    int i = 0;
+	  for(int i = t = 0; i<length;++i)
+	  {
+      if(word[i] == '_')
+		  {
+        word[i+1] = toUpperCase(word[i+1]);
+			  word[i] = '\0';
+		  }
+      else
+		  {
+			  word[t++] = word[i];
+		  }
+    }
+	  //Adjust the word
+	  word[0] = toLowerCase(word[0]);
+    int j = length - 1;
+    while(j >= t)
+    {
+      word[j] = '\0';
+      j--;
+    }
   }
-	//Adjust the word
-	word[0] = toLowerCase(word[0]);
-	for(int j = length - 1; j >= t;j--)
-	{
-		word[j] = '\0';
-	}	
-  //++i;
 }
 
 int main()
@@ -186,9 +203,10 @@ int main()
   
 	
 	// Print the new string
-  printf("After convertin: %s \n ", s);
+  printf("After converting: %s \n ", s);
 	
 	
 	return 0;
 }
+
 
